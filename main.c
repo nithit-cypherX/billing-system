bf14628fd6e5ce7cbf1c8c8f5ba201abc3020604
nithit-cypherX <eiang123nithit@gmail.com>
2023-08-16 15:32:37 +0700

    Add code

--- main.c
+++ main.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-    printf("Hello world!\n");
+struct user {
+    char phone[50];
+    char account[50];
+    char password[50];
+    float balance ;
+
+
+
+};
+
+int main(){
+    struct user usr ;
+    FILE *fp;
+    char filename[50];
+    int option;
+
+
+    printf("\nWhat do you want to do ?");
+    printf("\n\n1. Register an account ");
+    printf("\n2. login to an account");
+
+    printf("\n\nYour choice:\t");
+    scanf("%d",&option);
+
+    if(option == 1 ){
+        system("cls");
+        printf("Enter your account no: ");
+        scanf("%s",usr.account);
+        printf("\nEnter your phone number : ");
+        scanf("%s",usr.phone);
+        printf("\nEnter your new password : ");
+        scanf("%s",usr.password);
+        usr.balance = 0;
+        strcpy(filename,usr.phone); // copy phone number to be the name of filename
+        fp = fopen(strcat(filename,".dat"),"w"); // strcat connect two words between filename and .dat also w mean create/write file
+        fwrite(&usr,sizeof(struct user),1,fp);// write data into a file
+        8.33 minutes
+    }
+
     return 0;
 }
